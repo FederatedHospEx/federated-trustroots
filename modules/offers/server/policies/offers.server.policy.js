9c9855bcb6b67baf4ab300aa21919c21cee79b3e
Chagai <chagai95@gmail.com>
2021-10-07 12:49:42 +0200

    avoid checks for authentication until a better concept is made in policy

--- modules/offers/server/policies/offers.server.policy.js
+++ modules/offers/server/policies/offers.server.policy.js
@@ -57,11 +57,11 @@ exports.invokeRolesPolicies = function () {
  */
 exports.isAllowed = function (req, res, next) {
   // No offers for non-authenticated nor for authenticated but un-published users
-  if (!req.user || (req.user && !req.user.public)) {
+  /*   if (!req.user || (req.user && !req.user.public)) {
     return res.status(403).send({
       message: errorService.getErrorMessageByKey('forbidden'),
     });
-  }
+  } */
 
   // If an offer is being processed and the current user owns it, then allow any manipulation
   if (req.offer && req.user && req.offer.user === req.user._id) {
